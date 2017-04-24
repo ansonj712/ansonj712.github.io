3169e93a5b85cd2fbcc4461f2348d2157906d050
ans0nJ <ans0nJ72@gmail.com>
2017-04-24 16:58:13 -0700

    added particles.js

--- js/app.js
+++ js/app.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
         $('i').removeClass("fa-2x");
         $('.icon').css({'display' : 'inline', 'padding-right' : '0.1em'})
     }
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', '../particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
 });
